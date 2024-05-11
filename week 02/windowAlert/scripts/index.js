a9f6fb50ab74c7f0bb6f192c8111983d204abe53
Rik Decan <83696539+RikDecan@users.noreply.github.com>
2024-05-11 11:21:56 +0200

    ..

--- week 02/windowAlert/scripts/index.js
+++ week 02/windowAlert/scripts/index.js
@@ -1,15 +1,24 @@
 const setup = () => {
 
-    window.alert("Dit is een mededeling");
+    let resultaatAlert =window.alert("Dit is een mededeling");
     //Toont de mededeling in een popup met een 'ok' button
+    console.log(resultaatAlert);
 
-    window.confirm("Weet u het zeker?");
+
+
+    let resultaatConfirm = window.confirm("Weet u het zeker?");
     //Toont de vraag in een popup met een 'ok' en 'cancel' button
     // De return waarde geeft aan op welke knop de gebruiker duwde
+    console.log(resultaatConfirm);
 
-    window.prompt("Wat is uw naam", "onbekend");
+
+
+    let resultaatPrompt = window.prompt("Wat is uw naam", "onbekend");
     //Toont de vraag in een popup met een tekstveld met een 'ok' en een 'cancel' button.
     //De andere tekst ("onbekend" hierboven) wordt standaard al ingevuld bij het tonen.
+    console.log(resultaatPrompt);
+
+
 }
 
 window.addEventListener("load", setup);
