d6c0092804f1f4487ef163a3bc66e8ebcbb29a3a
Rik Decan <83696539+RikDecan@users.noreply.github.com>
2024-05-16 09:56:19 +0200

    ..

--- Portfolio/js deel 09/Opdracht01/scripts/index.js
+++ Portfolio/js deel 09/Opdracht01/scripts/index.js
@@ -2,26 +2,19 @@ const setup = () => {
     // koppel de 'verwijder' click event listener aan elk list item in onze lijst
     let items=document.querySelectorAll("#lstIngredients>li");
     for (let i=0;i<items.length;i++) {
-        items[i].addEventListener("click", verwijder);
+
+        items[i].addEventListener("click", deleteLi);
+
     }
 }
 
-const verwijder = (event) => {
-    // event.target zal het geklikte list item zijn
+const deleteLi  = (event) => {
     let item = event.target;
 
-    // de lijst is de parent van dit item
     let list = item.parentElement;
-    // We hadden natuurlijk ook
-    //    let list=document.querySelector("#lstIngredients");
-    // kunnen schrijven maar da's minder efficient als we het item al hebben.
-    // Plus, we willen ook eens .parentElement demonstreren :)
 
-    // verwijder het <li> element uit het <ul> element
     list.removeChild(item);
 
-    // zorg dat er geen navigatie plaatvindt door de klik op de hyperlink
-    event.preventDefault();
 }
 
 window.addEventListener('load', setup);
--- Portfolio/js deel 09/Opdracht02/scripts/index.js
+++ Portfolio/js deel 09/Opdracht02/scripts/index.js
@@ -1,29 +1,15 @@
 const setup = () => {
-    // koppel de 'verwijder' click event listener aan elke hyperlink in onze lijst
-    let verwijderLinks=document.querySelectorAll("#lstIngredients a");
-    for (let i=0;i<verwijderLinks.length;i++) {
-        verwijderLinks[i].addEventListener("click", verwijder);
+    let verwijderA=document.querySelectorAll("#lstIngredients a");
+    for (let i=0;i<verwijderA.length;i++) {
+        verwijderA[i].addEventListener("click", deleteLi);
     }
 }
 
-const verwijder = (event) => {
-    // event.target zal de geklikte hyperlink zijn
+const deleteLi = (event) => {
     let link = event.target;
-
-    // Nu moeten we het bovenliggende <li> element uit #lstIngredients verwijderen
-    // (de hyperlink verwijnt dan trouwens ook omdat die aan het verwijderde <li> element hangt).
-    //
-    // Om te verwijderen gebruiken we parent.removeChild(child), dus we hebben verwijzingen nodig
-    // 1) naar het <li> element, d.i. de parent van de hyperlink
-    // 2) naar het <ul> element, d.i. de parent van het list item
     let item = link.parentElement;
-    let list = item.parentElement;  // we hadden natuurlijk ook 'let list=document.querySelector("#lstIngredients");' kunnen schrijven maar da's minder efficient
-
-    // verwijder het <li> element uit het <ul> element
+    let list = item.parentElement;  
     list.removeChild(item);
-
-    // zorg dat er geen navigatie plaatvindt door de klik op de hyperlink
-    event.preventDefault();
 }
 
 window.addEventListener('load', setup);
